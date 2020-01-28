6d8db674e8239423205a3ee553bb79ca1b218516
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-28 18:14:57 -0500

    foo

--- .github/actions/gcloud/entrypoint.sh
+++ .github/actions/gcloud/entrypoint.sh
@@ -4,7 +4,6 @@ set -eu
 
 # Install gcloud and kubectl.
 echo "$INPUT_CLOUD_SDK_SERVICE_ACCOUNT_KEY" > .gcp.json
-echo $HOME
 dir="${CLOUDSDK_INSTALL_DIR:-${HOME}}/google-cloud-sdk"
 (
     . bin/_gcp.sh ;
@@ -16,7 +15,8 @@ dir="${CLOUDSDK_INSTALL_DIR:-${HOME}}/google-cloud-sdk"
     get_k8s_ctx "$INPUT_GCP_PROJECT" "$INPUT_GCP_ZONE" "$INPUT_CLUSTER"
 )
 . "$dir/path.bash.inc"
-echo $PATH
-gcloud info --format='get(config.paths.active_config_path)'
 gcloud auth configure-docker
 bin/kubectl version --short
+
+# so that it's modifiable by the runner's user
+chown -R 777 ~HOME/.config/gcloud
