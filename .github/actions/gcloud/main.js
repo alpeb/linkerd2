1d9e1a53b67595a39b6ceef0c992ffbe4a748814
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-29 10:14:13 -0500

    foo

--- .github/actions/gcloud/main.js
+++ .github/actions/gcloud/main.js
@@ -8,11 +8,11 @@ try {
     if (err) throw err;
   }); 
 
-  exec.exec(`gcloud auth activate-service-account --key-file ${process.env.HOME}/.gcp.json`);
-  exec.exec(`gcloud config set core/project "${core.getInput('gcp_project')}"`);
-  exec.exec(`gcloud config set compute/zone "${core.getInput('gcp_zone')}"`);
-  exec.exec(`gcloud config set container/cluster "${core.getInput('cluster')}"`);
-  exec.exec(`gcloud container clusters get-credentials "${core.getInput('cluster')}"`);
+  await exec.exec(`gcloud auth activate-service-account --key-file ${process.env.HOME}/.gcp.json`);
+  await exec.exec(`gcloud config set core/project "${core.getInput('gcp_project')}"`);
+  await exec.exec(`gcloud config set compute/zone "${core.getInput('gcp_zone')}"`);
+  await exec.exec(`gcloud config set container/cluster "${core.getInput('cluster')}"`);
+  await exec.exec(`gcloud container clusters get-credentials "${core.getInput('cluster')}"`);
 } catch (error) {
   core.setFailed(error.message);
 }
