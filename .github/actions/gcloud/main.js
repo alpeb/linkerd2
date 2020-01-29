df51e2ea3dc2502817159703074392e48791cdd9
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-29 10:35:05 -0500

    foo

--- .github/actions/gcloud/main.js
+++ .github/actions/gcloud/main.js
@@ -4,13 +4,12 @@ const exec = require('@actions/exec');
 var fs = require('fs');
 
 async function configure() {
-    await exec.exec(`sudo apt-get install kubectl`);
-    
     await exec.exec(`gcloud auth activate-service-account --key-file ${process.env.HOME}/.gcp.json`);
     await exec.exec(`gcloud config set core/project "${core.getInput('gcp_project')}"`);
     await exec.exec(`gcloud config set compute/zone "${core.getInput('gcp_zone')}"`);
     await exec.exec(`gcloud config set container/cluster "${core.getInput('cluster')}"`);
     await exec.exec(`gcloud container clusters get-credentials "${core.getInput('cluster')}"`);
+    await exec.exec('kubectl version');
 }
 
 try {
