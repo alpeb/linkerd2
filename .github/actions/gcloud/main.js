acb95621d055e86eac129ad53cae3856f33e4a44
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-29 09:50:04 -0500

    foo

--- .github/actions/gcloud/main.js
+++ .github/actions/gcloud/main.js
@@ -5,12 +5,12 @@ const exec = require('@actions/exec');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(__dirname)
-  console.log(process.env.HOME)
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(__dirname);
+  console.log(process.env.HOME);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 
-  await exec.exec('echo "' + core.getInput('cloud_sdk_service_account_key') + '" > ' + process.env.HOME + '/.gcp.json')
+  await exec.exec('echo "' + core.getInput('cloud_sdk_service_account_key') + '" > ' + process.env.HOME + '/.gcp.json');
   //echo "$CLOUD_SDK_SERVICE_ACCOUNT_KEY" > .gcp.json
   //await exec.exec('gcloud auth activate-service-account --key-file .gcp.json');
 } catch (error) {
