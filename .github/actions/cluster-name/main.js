0367dbdc0ee69e29dfbe5f226dd2ead2642604a1
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-29 17:44:43 -0500

    foo

---
+++ .github/actions/cluster-name/main.js
@@ -0,0 +1,43 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+    var tag;
+    await exec.exec('bin/root-tag', [], {
+        env: {
+            CI_FORCE_CLEAN: 1
+        },
+        listeners: {
+            stdout: (data) => {
+                tag = data.toString()
+            }
+        }
+    });
+
+    var clientVersion;
+    await exec.exec('target/cli/linux/linkerd', ['--client', '--short'], {
+        listeners: {
+            stdout: (data) => {
+                clientVersion = data.toString()
+            }
+        }
+    });
+
+    // validate CLI version matches the repo
+    if (tag != clientVersion) {
+        core.setFailed(`tag ${tag} differs from clientversion ${clientVersion}`)
+    }
+    console.log('Installed Linkerd CLI version', tag)
+
+    // last part is to distinguish runs on the same sha
+    var name = `testing-${tag}-${core.getInput('run-id')}`;
+    console.log('name', name);
+    core.setOutput('name', name);
+}
+
+try {
+    console.log('run-id', github.context.run_id);
+    run()
+} catch (error) {
+    core.setFailed(error.message);
+}
