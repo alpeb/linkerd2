2af87b6178e3aed42d1a1d575be8c8df012303d1
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-29 09:37:23 -0500

    foo

--- .github/actions/gcloud/main.js
+++ .github/actions/gcloud/main.js
@@ -1,16 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
   console.log(__dirname)
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
+  console.log(process.env.HOME)
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  //await exec.exec('gcloud auth activate-service-account --key-file .gcp.json');
 } catch (error) {
   core.setFailed(error.message);
 }
