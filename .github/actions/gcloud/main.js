96ef1121e23541849df76614e8c879f795a85869
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-29 15:03:15 -0500

    foo

--- .github/actions/gcloud/main.js
+++ .github/actions/gcloud/main.js
@@ -9,6 +9,14 @@ async function configure() {
     await exec.exec(`gcloud config set container/cluster "${core.getInput('cluster')}"`);
     await exec.exec(`gcloud container clusters get-credentials "${core.getInput('cluster')}"`);
     await exec.exec('gcloud auth configure-docker --quiet');
+    await exec.exec('gcloud config get-value account', {
+      listeners: {
+        stdout: (data) => {
+          console.log("DATA: ", data.toString())
+        }
+      }
+    });
+    await exec.exec(`kubectl create clusterrolebinding ci-cluster-admin --clusterrole=cluster-admin --user=test-ci-eraseme@linkerd-io.iam.gserviceaccount.com`);
 }
 
 try {
