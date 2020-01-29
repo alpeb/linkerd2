6648b143b9c3e7772385aafdf43a7c9505c19aed
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-29 10:18:23 -0500

    foo

--- .github/actions/gcloud/main.js
+++ .github/actions/gcloud/main.js
@@ -3,14 +3,18 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 var fs = require('fs');
 
-try {
-  fs.writeFile(process.env.HOME + '/.gcp.json', core.getInput('cloud_sdk_service_account_key'), function (err) {
+async function configure() {
     await exec.exec(`gcloud auth activate-service-account --key-file ${process.env.HOME}/.gcp.json`);
     await exec.exec(`gcloud config set core/project "${core.getInput('gcp_project')}"`);
     await exec.exec(`gcloud config set compute/zone "${core.getInput('gcp_zone')}"`);
     await exec.exec(`gcloud config set container/cluster "${core.getInput('cluster')}"`);
     await exec.exec(`gcloud container clusters get-credentials "${core.getInput('cluster')}"`);
+}
+
+try {
+  fs.writeFile(process.env.HOME + '/.gcp.json', core.getInput('cloud_sdk_service_account_key'), function (err) {
+      configure()
   }); 
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
