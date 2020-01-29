44236f93a557bf4b463df8bc48cb5c8f66ce476e
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-29 10:12:44 -0500

    foo

--- .github/actions/gcloud/main.js
+++ .github/actions/gcloud/main.js
@@ -4,22 +4,15 @@ const exec = require('@actions/exec');
 var fs = require('fs');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(__dirname);
-  console.log(process.env.HOME);
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  //console.log(`The event payload: ${payload}`);
-
   fs.writeFile(process.env.HOME + '/.gcp.json', core.getInput('cloud_sdk_service_account_key'), function (err) {
     if (err) throw err;
   }); 
 
-  //exec.exec('echo "' + core.getInput('cloud_sdk_service_account_key') + '" > ' + process.env.HOME + '/.gcp.json');
-  //exec.exec('cat /home/runner/.gcp.json');
-  //exec.exec('node -version');
-  //echo "$CLOUD_SDK_SERVICE_ACCOUNT_KEY" > .gcp.json
   exec.exec(`gcloud auth activate-service-account --key-file ${process.env.HOME}/.gcp.json`);
+  exec.exec(`gcloud config set core/project "${core.getInput('gcp_project')}"`);
+  exec.exec(`gcloud config set compute/zone "${core.getInput('gcp_zone')}"`);
+  exec.exec(`gcloud config set container/cluster "${core.getInput('cluster')}"`);
+  exec.exec(`gcloud container clusters get-credentials "${core.getInput('cluster')}"`);
 } catch (error) {
   core.setFailed(error.message);
 }
