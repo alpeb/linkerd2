66146af52787c199b9335559b3940c9bbccece29
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-30 08:03:08 -0500

    foo

--- .github/actions/gcloud2/entrypoint.sh
+++ .github/actions/gcloud2/entrypoint.sh
@@ -9,10 +9,9 @@ dir="${CLOUDSDK_INSTALL_DIR:-${HOME}}/google-cloud-sdk"
     . bin/_gcp.sh ;
     install_gcloud "$dir"
     gcloud components install kubectl
+    set_gcloud_config "$INPUT_GCP_PROJECT" "$INPUT_GCP_ZONE" foobar
 )
 . "$dir/path.bash.inc"
 gcloud auth configure-docker
-echo $GITHUB_WORKSPACE
 echo "::add-path::/home/runner/work/_temp/_github_home/google-cloud-sdk/bin"
-echo $PATH
-gcloud version
+gcloud config list
