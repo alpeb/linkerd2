6836e616366e0e40ec7aa13e7a0fbce59d787a60
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-30 06:16:12 -0500

    foo

--- .github/actions/gcloud/main.js
+++ .github/actions/gcloud/main.js
@@ -34,7 +34,7 @@ async function getClusterName() {
   // Last part is to distinguish runs on the same sha (run-id is unique per CI run).
   // run-id has to be provided as an input because it turns out it's not available
   // through github.context.run_id
-  console.log('****** RUN ID:', github.run_id);
+  console.log('****** GITHUB:', github);
   var name = `testing-${tag}-${core.getInput('run-id')}`;
   console.log('Cluster name: ', name);
   return name;
