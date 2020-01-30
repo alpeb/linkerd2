d67f37267227022b6c3aef12e35163851c8ec5d4
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-30 06:52:06 -0500

    foo

--- .github/actions/gcloud2/entrypoint.sh
+++ .github/actions/gcloud2/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-gcloud version
+set -eu
+
+# Install gcloud and kubectl.
+echo "$INPUT_CLOUD_SDK_SERVICE_ACCOUNT_KEY" > .gcp.json
+dir="${CLOUDSDK_INSTALL_DIR:-${HOME}}/google-cloud-sdk"
+(
+    . bin/_gcp.sh ;
+    install_gcloud "$dir"
+    gcloud components install kubectl
+)
+. "$dir/path.bash.inc"
+gcloud auth configure-docker
