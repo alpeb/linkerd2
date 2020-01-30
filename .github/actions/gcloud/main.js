92b3a10c0cba55a45207640a3b8502fdc14f9756
Alejandro Pedraza <alejandro@buoyant.io>
2020-01-30 13:21:56 -0500

    foo

--- .github/actions/gcloud/main.js
+++ .github/actions/gcloud/main.js
@@ -15,7 +15,7 @@ async function getClusterName() {
       }
   });
 
-  await exec.exec(`${process.env.HOME}/.linkerd`, ['version', '--client', '--short'], {
+  await exec.exec(`${process.env.HOME}/.linkerd version --client --short`, [], {
       listeners: {
           stdout: (data) => {
               clientVersion = data.toString().trim()
@@ -25,7 +25,7 @@ async function getClusterName() {
 
   // validate CLI version matches the repo
   if (tag != clientVersion) {
-      core.setFailed(`tag ${tag} differs from clientversion ${clientVersion}`)
+      throw `tag ${tag} differs from clientversion ${clientVersion}`
   }
   console.log('Linkerd CLI version:', tag)
 
@@ -33,22 +33,24 @@ async function getClusterName() {
   // run-id has to be provided as an input because it turns out it's not available
   // through github.context.run_id
   const name = `testing-${tag}-${core.getInput('run-id')}`;
-  console.log('Cluster name: ', name);
+  console.log('Cluster name:', name);
   return name;
 }
 
 async function configure() {
-    await exec.exec(`gcloud auth activate-service-account --key-file ${process.env.HOME}/.gcp.json`);
-    await exec.exec(`gcloud config set core/project "${core.getInput('gcp_project')}"`);
-    await exec.exec(`gcloud config set compute/zone "${core.getInput('gcp_zone')}"`);
+    await exec.exec('gcloud auth activate-service-account', ['--key-file',  `${process.env.HOME}/.gcp.json`]);
+    await exec.exec('gcloud config set core/project', [core.getInput('gcp_project')]);
+    await exec.exec('gcloud config set compute/zone', [core.getInput('gcp_zone')]);
     await exec.exec('gcloud auth configure-docker --quiet');
 
     if (core.getInput('create') || core.getInput('destroy')) {
       const name = await getClusterName();
       if (core.getInput('create')) {
-        await exec.exec(`gcloud container clusters create ${name} --cluster-version 1.15.7-gke.23 --num-nodes=1 --machine-type n1-standard-2 --enable-network-policy`);
-        await exec.exec(`gcloud config set container/cluster "${name}"`);
-        await exec.exec(`gcloud container clusters get-credentials "${name}"`);
+        await exec.exec('gcloud container clusters create',
+          [name, '--cluster-version 1.15.7-gke.23 --num-nodes=1 --machine-type n1-standard-2 --enable-network-policy']);
+        await exec.exec('gcloud config set container/cluster',  [name]);
+        await exec.exec('gcloud container clusters get-credentials', [name]);
+
         let sa;
         await exec.exec('gcloud config get-value account', [], {
           listeners: {
@@ -57,9 +59,9 @@ async function configure() {
             }
           }
         });
-        await exec.exec(`kubectl create clusterrolebinding ci-cluster-admin --clusterrole=cluster-admin --user=${sa}`);
+        await exec.exec('kubectl create clusterrolebinding ci-cluster-admin --clusterrole=cluster-admin', [`--user=${sa}`]);
       } else {
-        await exec.exec(`gcloud container clusters delete ${name} --quiet`)
+        await exec.exec('gcloud container clusters delete --quiet', [name]);
       }
     }
 }
