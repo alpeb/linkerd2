f56f56d63c495b6847571bdcc0fb47fb6d5c2df4
Alejandro Pedraza <alejandro@buoyant.io>
2020-02-04 14:25:36 -0500

    foobar

---
+++ .github/actions/gcloud/configure.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const fs = require('fs');
+
+export function writeKey() {
+  fs.writeFileSync(process.env.HOME + '/.gcp.json', core.getInput('cloud_sdk_service_account_key'));
+}
+
+export async function gcloud() {
+  try {
+    await exec.exec('gcloud auth activate-service-account',
+      ['--key-file',  `${process.env.HOME}/.gcp.json`]);
+    await exec.exec('gcloud config set core/project', [core.getInput('gcp_project')]);
+    await exec.exec('gcloud config set compute/zone', [core.getInput('gcp_zone')]);
+    await exec.exec('gcloud auth configure-docker --quiet');
+  } catch (e) {
+    core.setFailed(e.message)
+  }
+}
+
