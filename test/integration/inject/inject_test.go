710dc3b06f6077a000809b2b46e158a555b9ec2e
Alejandro Pedraza <alejandro@buoyant.io>
2021-08-30 12:44:08 -0500

    fixed another injection test

--- test/integration/inject/inject_test.go
+++ test/integration/inject/inject_test.go
@@ -382,7 +382,7 @@ func TestInjectAutoPod(t *testing.T) {
 	zero := int64(0)
 	expectedInitContainer := v1.Container{
 		Name:  k8s.InitContainerName,
-		Image: "cr.l5d.io/linkerd/proxy-init:" + version.ProxyInitVersion,
+		Image: "ghcr.io/linkerd/proxy-init:" + version.ProxyInitVersion,
 		Args: []string{
 			"--incoming-proxy-port", "4143",
 			"--outgoing-proxy-port", "4140",
