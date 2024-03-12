38406acd78321d95e3b31f7cebf9bee9d03c75e9
Alejandro Pedraza <alejandro@buoyant.io>
2024-03-12 16:02:09 -0500

    test1

--- test/integration/multicluster/install_test.go
+++ test/integration/multicluster/install_test.go
@@ -113,7 +113,7 @@ func TestInstall(t *testing.T) {
 	cmd = []string{
 		"install",
 		"--controller-log-level", "debug",
-		"--set", "proxyInit.image.name=ghcr.io/linkerd/proxy-init",
+		"--set", "proxyInit.image.name=ghcr.io/alpeb/proxy-init",
 		"--set", fmt.Sprintf("proxyInit.image.version=%s", version.ProxyInitVersion),
 		"--set", fmt.Sprintf("proxy.image.version=%s", TestHelper.GetVersion()),
 		"--set", "heartbeatSchedule=1 2 3 4 5",
